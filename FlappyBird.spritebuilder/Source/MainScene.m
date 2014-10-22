f28bb0eb46fa916513492e83315e06e21e1555d8
Heidi Joy Cha <Heidi@Heidis-MacBook-Pro-3.local>
2014-10-21 20:39:24 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -10,13 +10,6 @@
 #import "Obstacle.h"
 
 @implementation MainScene {
-    
-    CGPoint _cloudParallaxRatio;
-    CGPoint _bushParallaxRatio;
-    
-    CCNode *_parallaxContainer;
-    CCParallaxNode *_parallaxBackground;
-    
     CCNode *_ground1;
     CCNode *_ground2;
     NSArray *_grounds;
